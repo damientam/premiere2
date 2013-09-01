77859ad1b091b94d3a5d16249d8df86f066e0c81
damient <dtampe@gmail.com>
2013-09-02 01:28:34 +0200

    install premiere2 avec ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Premiere::Application.config.secret_key_base = '6e59667a1abb56070ed20063e5c9fd73d667866510af1084cf37b3bf44994c1ee20fe612b8d993f047d664b50f61d4d8c548795aaf63e2d9cd248422d36e4719'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Premiere::Application.config.session_store :cookie_store, key: '_premiere_session'
